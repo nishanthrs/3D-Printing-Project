d8f238b1bec5fefea38c18d088431bf959b95349
nishanthrs <nishanth.salinamakki@gmail.com>
2016-05-20 02:20:38 -0700

    fourth commit

--- 3DPrintingCoordinateCalculation.py
+++ 3DPrintingCoordinateCalculation.py
@@ -4,24 +4,39 @@ import sys
 xValues = [];     
 yValues = [];
 zValues = [];
+extrusionValues = [];
 
+x1 = -1;
+y1 = -1;
 firstBound = 1;       ### starting x Bound 
 finalBound = 10;      ### starting y Bound
 deltaX = (finalBound - firstBound) * .1; ###steps for the x-values 
+extrusionValue = 5;
+
+def calculateExtrusion(x, y):
+	distance = (((x1-x)*(x1-x)) + ((y1-y)*(y1-y)))**0.5;
+	return distance * 0.1;
 
 ### For x^2
-for x in np.arange(firstBound, finalBound, .1): ## for x in range(firstxBound, finalxBound):
+for x in np.arange(firstBound, finalBound, .05): ## for x in range(firstxBound, finalxBound):
 	for i in range(1,60): ## Range must be twice the unit of measurement to rotate around 2*pi (full circle)
 		xValue = x * np.cos(np.pi*i/30) + 50; ## Rotate x point by using cosine 
 		yValue = x * np.sin(np.pi*i/30) + 50; ## Rotate y point by using sine 
-		zValue = x**0.5;                  ## z point is simply elevation or function
+		zValue = x**2;                  ## z point is simply elevation or function
 		xValues.append(xValue); 
 		yValues.append(yValue);
 		zValues.append(zValue);
+		if (x1 != -1 and y1 != -1):
+			extrusionValue += calculateExtrusion(xValue, yValue);
+			extrusionValues.append(extrusionValue); 
+		## Previous values used to calculate distance between points and thus, extrusion values
+		x1 = xValue; 
+		y1 = yValue; 
 
 print(xValues);
 print(yValues);
 print(zValues);
+print(extrusionValues);
 
 gCodeString = "G90\n";
 gCodeString += "M107\n";
@@ -36,11 +51,12 @@ gCodeString += "M82\n";
 gCodeString += "G1 F1800.000 E-1.00000\n";
 gCodeString += "G92 E0\n";
 
-for num in range(1, 5310):
+for num in range(1, 10600): ## 5309 with 0.1
 	## E part of GCode is calculated via formula (do later)
-	gCodeString += "G1" + " " + "X" + str(xValues[num]) + " " + "Y" + str(yValues[num]) + " " + "Z" + str(zValues[num]) + " " + "E" + "5.0" + " " + "F" + "2.5" + "\n";
+	gCodeString += "G1" + " X" + str(xValues[num]) + " Y" + str(yValues[num]) + " Z" + str(zValues[num]) + " E" + str(extrusionValues[num]) + " F" + "2.5" + "\n";
 
 print gCodeString;	
+## str(extrusionValues[num])
 
 def write():
 
