562840fb0a98bfd996f54dc42c9e135c03af8f5d
nishanthrs <nishanth.salinamakki@gmail.com>
2016-05-25 22:03:33 -0700

    seventh commit

--- FunctionInputUI.py
+++ FunctionInputUI.py
@@ -16,17 +16,14 @@ label.grid(row = 1, column = 0, sticky = W);
 function = Entry(app);
 function.grid(row = 3, column = 0, sticky = W);
 
-v = IntVar();
-
-xRevolveRadioButton = Radiobutton(root, text = "Revolve about x-axis", variable=v, value=1);
-xRevolveRadioButton.grid(row = 1, column = 2, sticky = W);
-yRevolveRadioButton = Radiobutton(root, text = "Revolve about y-axis", variable=v, value=2);
-yRevolveRadioButton.grid(row = 2, column = 2, sticky = W);
+firstxBound = Entry(app);
+firstxBound.grid(row = 0, column = 2, sticky = W);
+finalxBound = Entry(app);
+finalxBound.grid(row = 1, column = 2, sticky = W);
 
 def generateGCode():
 	print("FUNCTION: " + function.get());
-	print(v.get());
-	gCodeString = createFunctionPointsAndGCode(function.get(), v.get());
+	gCodeString = createFunctionPointsAndGCode(function.get(), firstxBound.get(), finalxBound.get());
 	write(gCodeString);
 
 generateGCodeButton = Button(app, text = "Generate", command = generateGCode);
--- PrintingCoordinateCalculation.py
+++ PrintingCoordinateCalculation.py
@@ -1,28 +1,11 @@
 import numpy as np
 import sys
 
-## global xValues; 
-## xValues = [];    
-## global yValues;
-## yValues = [];
-## global zValues;
-## zValues = [];
-## global extrusionValues;
-## extrusionValues = [];
-
-## global extrusionValue;
-## extrusionValue = 5;
-
-## global x1
-## x1 = -1;
-## global y1
-## y1 = -1;
-
 def calculateExtrusion(x, y, x1, y1):
 	distance = (((x1-x)*(x1-x)) + ((y1-y)*(y1-y)))**0.5;
 	return distance * 0.1;
 
-def createFunctionPointsAndGCode(function, axis):
+def createFunctionPointsAndGCode(function, firstBound, finalBound):
 	x1 = -1;
 	y1 = -1;
 
@@ -35,25 +18,25 @@ def createFunctionPointsAndGCode(function, axis):
 
 	axis = 2;
 
-	for x in np.arange(1, 10, .1): ## for x in range(firstxBound, finalxBound):
+	iterationArray = np.arange(int(firstBound), int(finalBound), .005);
+
+	for x in iterationArray: ## for x in range(firstxBound, finalxBound):
 		for i in range(1,60): ## Range must be twice the unit of measurement to rotate around 2*pi (full circle)
 			xValue = x * np.cos(np.pi*i/30) + 50; ## Rotate x point by using cosine 
 			yValue = x * np.sin(np.pi*i/30) + 50; ## Rotate y point by using sine 
-			zValue = eval(function);                  ## z point is simply elevation or function
+			zValue = -1 * eval(function);         ## z point is simply elevation or function
+			## xValue = eval(function);
+			## yValue = x * np.sin(np.pi*i/30) + 50;
+			## zValue = x * np.cos(np.pi*i/30) + 50;
 			xValues.append(xValue); 
 			yValues.append(yValue);
 			zValues.append(zValue);
 			if (x1 != -1 and y1 != -1):
-				extrusionValue += calculateExtrusion(xValue, yValue, x1, y1);
+				extrusionValue = calculateExtrusion(xValue, yValue, x1, y1);
 				extrusionValues.append(extrusionValue); 
 			## Previous values used to calculate distance between points and thus, extrusion values 
 			x1 = xValue; 
 			y1 = yValue;
-
-	##print(xValues);
-	##print(yValues);
-	##print(zValues);
-	##print(extrusionValues);
 
 	gCodeString = "G90\n";
 	gCodeString += "M107\n";
@@ -68,14 +51,13 @@ def createFunctionPointsAndGCode(function, axis):
 	gCodeString += "G1 F1800.000 E-1.00000\n";
 	gCodeString += "G92 E0\n";
 
-	for num in range(1, 5309): ## 5309 with 0.1
-		## E part of GCode is calculated via formula (do later)
-		gCodeString += "G1" + " X" + str(xValues[num]) + " Y" + str(yValues[num]) + " Z" + str(zValues[num]) + " E" + str(extrusionValues[num]) + " F" + "2.5" + "\n";
+	for num in range(1, (int(finalBound)-int(firstBound))*200*59-1): ## 5309 with 0.1
+		maxHeight = -1 * zValues[(int(finalBound)-int(firstBound))*200*59-1]; ## merely a constant
+		print("MAX HEIGHT: " + str(maxHeight));
+		gCodeString += "G1" + " X" + str(xValues[num]) + " Y" + str(yValues[num]) + " Z" + str(zValues[num] + maxHeight) + " E" + str(extrusionValues[num]) + " F" + "2.5" + "\n";
 
 	print "G CODE: " + gCodeString;	
 	return gCodeString;
-	## str(extrusionValues[num])
-
 
 def write(gCode):
 
